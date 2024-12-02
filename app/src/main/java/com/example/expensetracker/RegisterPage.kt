56892c26051e9721c58784d88d1c3225b0646d8c
jen <91272778+jennefercampoverde@users.noreply.github.com>
2024-12-02 10:26:02 -0500

    Added intents/viewbinding/map creation for start page and register page

--- app/src/main/java/com/example/expensetracker/RegisterPage.kt
+++ app/src/main/java/com/example/expensetracker/RegisterPage.kt
@@ -1,20 +1,31 @@
 package com.example.expensetracker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.expensetracker.databinding.ActivityRegisterPageBinding
 
 class RegisterPage : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val users=emptyMap<String, String>()
+        binding.registerSubmitButton.setOnClickListener(){
+                //grab what user submitted to edittext and save to variable
+
+
+            val i= Intent(this,MainActivity::class.java)
+            i.putExtra("username","hei")
+            i.putExtra("password","hei")
+            startActivity(i)
+
         }
     }
 }
---
+++ app/src/main/java/com/example/expensetracker/StartPage.kt
@@ -0,0 +1,42 @@
+package com.example.expensetracker
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.expensetracker.databinding.ActivityStartPageBinding
+
+class StartPage : AppCompatActivity() {
+    private lateinit var binding: ActivityStartPageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.loginButton.setOnClickListener(){
+        //adding intent calling login acivity so user can log into their account
+            val i = Intent(this, LoginPage::class.java)
+            startActivity(i)
+
+
+
+        }
+        binding.registerButton.setOnClickListener(){
+            //add intent calling the register acivity so user can register for an account
+            val i = Intent(this, RegisterPage::class.java)
+            startActivity(i)
+        }
+
+
+    }
+}
